936f843d7bd2c9069b6181f5de3dd182a93dbd09
Erik Herrera <erik.herrera@unosquare.com>
2022-11-24 22:40:14 -0600

    Second commit

---
+++ SDETAPI_CSharp/Features/IFeature.cs
@@ -0,0 +1,14 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDETAPI_CSharp.Features
+{
+    public interface IFeature
+    {
+        public string RequestsDirectory { get; }
+
+        public abstract void SetupQueryParameters(RestRequest request, List<KeyValuePair<string, string>> queryParameters = null);
+    }
+}
--- SDETAPI_CSharp/Tests/NASATests.cs
+++ SDETAPI_CSharp/Tests/NASATests.cs
@@ -1,19 +1,61 @@
 using NUnit.Framework;
+using RestSharp;
+using SDETAPI_CSharp.Features;
+using SDETAPI_CSharp.Features.NasaOpenAPI;
+using System.Collections.Generic;
+using System.Net;
 
 namespace SDETAPI_CSharp.Tests
 {
     public class NASATests
     {
+        private RequestBuilder<NasaFeature> _requestBuilder;
+
         [SetUp]
         public void Setup()
         {
-
+            this._requestBuilder = new RequestBuilder<NasaFeature>();
         }
 
         [Test]
-        public void Test1()
+        public void GetApodTest()
         {
-            Assert.Pass();
+            var response = this._requestBuilder.PerformRequest("apod");
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+        [Test]
+        public void GetAsteroidsNewWsTest()
+        {
+            var response = this._requestBuilder.PerformRequest("asteroidsNewWs");
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+        [Test]
+        public void GetTechTransferTest()
+        {
+            var queryParams = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("engine", "")
+            };
+            var resourceList = new List<string>()
+            {
+                "patent",
+                "patent_issued",
+                "software",
+                "spinoff"
+            };
+
+            foreach(var resource in resourceList)
+            {
+                var response = this._requestBuilder.PerformRequest(
+                    "techTransfer",
+                    queryParams: queryParams,
+                    customAction: r => r.AddUrlSegment("resource", resource)
+                );
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            }
+
         }
     }
 }
